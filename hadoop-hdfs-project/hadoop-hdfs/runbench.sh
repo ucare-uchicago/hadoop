aafef4fa46d543d5d6c7280a15491efa994cdfbe
Riza Suminto <riza.on@gmail.com>
2017-03-28 20:25:04 +0000

    edit logging for easier grep

--- hadoop-hdfs-project/hadoop-hdfs/runbench.sh
+++ hadoop-hdfs-project/hadoop-hdfs/runbench.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-hadoop jar $HADOOP_PREFIX/share/hadoop/hdfs/hadoop-hdfs-2.7.1-tests.jar org.apache.hadoop.hdfs.server.namenode.NNThroughputBenchmark -op blockReport -reports 1 -blocksPerReport 10000 -blocksPerFile 106 -writerPoolSize 1 -datanodes 32
+clsetup
+hdfs namenode -format -nonInteractive
+hadoop jar $HADOOP_PREFIX/share/hadoop/hdfs/hadoop-hdfs-2.7.1-tests.jar org.apache.hadoop.hdfs.server.namenode.NNThroughputBenchmark -op blockReport -reports 1 -blocksPerReport $1 -blocksPerFile $3 -writerPoolSize $4 -datanodes $2
