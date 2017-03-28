96f0d9b8ed6c5672f7ba02676ea0b32906284f0e
Riza Suminto <riza.on@gmail.com>
2017-03-28 20:02:05 +0000

    copy new hdfs jar after compilation

--- hadoop-hdfs-project/hadoop-hdfs/compile.sh
+++ hadoop-hdfs-project/hadoop-hdfs/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mvn package install -Pdist -DskipTests
+cp target/hadoop-hdfs-2.7.1.jar /home/cc/hadoop-2.7.1/hadoop-dist/target/hadoop-2.7.1/share/hadoop/hdfs/hadoop-hdfs-2.7.1.jar
 cp target/hadoop-hdfs-2.7.1-tests.jar /home/cc/hadoop-2.7.1/hadoop-dist/target/hadoop-2.7.1/share/hadoop/hdfs/hadoop-hdfs-2.7.1-tests.jar
