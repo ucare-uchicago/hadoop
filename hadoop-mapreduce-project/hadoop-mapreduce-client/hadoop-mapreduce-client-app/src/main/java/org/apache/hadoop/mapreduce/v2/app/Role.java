883ddd4c4d20d89efd5585147b6b5e768c180696
Riza Suminto <riza.on@gmail.com>
2017-08-16 15:33:13 -0600

    move package and tidy up stuff

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/Role.java
+++
@@ -1,8 +0,0 @@
-package org.apache.hadoop.mapreduce.v2.app;
-
-/**
- * Created by huanke on 11/27/16.
- */
-public enum Role {
-    RM, NM, AM
-}
--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/InterceptEventType.java
+++
@@ -1,8 +0,0 @@
-package org.apache.hadoop.yarn.server.nodemanager;
-
-/**
- * Created by huanke on 11/27/16.
- */
-public enum InterceptEventType {
-    NodeStatusUpdate
-}
