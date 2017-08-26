f1eccb9da49ca6cb0f1c64be5748912c420f78a0
Riza Suminto <riza.on@gmail.com>
2017-08-26 17:29:34 +0000

    add requirement on chameleon setup

--- psbin/chameleon/dmck-req.sh
+++ psbin/chameleon/dmck-req.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y tmux git openjdk-7-jdk protobuf-compiler libprotobuf-java maven
+sudo apt-get install -y tmux htop git openjdk-7-jdk protobuf-compiler libprotobuf-java maven ant
 
