8942dcc48b01dc950c74154b9495b76825ee5f38
Ubuntu <ubuntu@nfshost.ucare.edu>
2017-09-14 18:58:27 +0000

    commit config files before branching

--- psbin/ucare_se-rc.sh
+++ psbin/ucare_se-rc.sh
@@ -8,8 +8,8 @@ export TESTDIR=${SWIMDIR}/workloadSuite/generatedWorkloads/st-FB2010_Proper_30no
 
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
-export HADOOP_PREFIX=${PR}/hadoop-2.7.1
-export HADOOP_CONF_DIR=${DIR}/ucare_se_conf/hadoop-etc/hadoop-2.7.1
+export HADOOP_PREFIX=/home/ubuntu/hadoop/hadoop-dist/target/hadoop-2.1.0-beta
+export HADOOP_CONF_DIR=${DIR}/ucare_se_conf/hadoop-etc/hadoop-2.1.0-beta
 export HADOOP_HOME=${HADOOP_PREFIX}
 export HADOOP_LOG_DIR=/tmp/hadoop-ucare/logs/hadoop
 export YARN_LOG_DIR=/tmp/hadoop-ucare/logs/yarn
@@ -19,12 +19,13 @@ export PSBIN=${DIR}
 export PATH=${HADOOP_PREFIX}/bin:${HADOOP_PREFIX}/sbin:${PSBIN}:${PATH}
 
 alias h="history 25"
+alias ls="ls --color"
 alias la="ls -a"
 alias lf="ls -FA"
 alias ll="ls -lA"
 
 alias pr="cd $PR"
-alias e="emacs"
+alias e="vim"
 alias hp="cd $HADOOP_PREFIX"
 alias hconf="cd $HADOOP_CONF_DIR"
 alias hlogs="cd /tmp/logs/yarn/userlogs/"
@@ -35,3 +36,5 @@ alias ben="cd $TESTDIR/workGenLogs/"
 alias mjl="mapred job -list"
 alias mjs="mapred job -status"
 alias psbin="cd $PSBIN"
+alias samc="cd ~/samc"
+alias exp="cd /tmp/dmck-hadoop"
