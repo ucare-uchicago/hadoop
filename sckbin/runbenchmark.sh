017253c907391ecd4f140f1eb7e4a89cdbab20b4
Riza Suminto <riza.on@gmail.com>
2018-04-27 21:29:06 +0000

    add some scripts

--- sckbin/runbenchmark.sh
+++ sckbin/runbenchmark.sh
@@ -1,13 +1,12 @@
 #/bin/bash
 
-rm -rf ../hadoop-hdfs-project/hadoop-hdfs/target/test/data/
-
 # set max heap size 64GB
-export HADOOP_HEAPSIZE=65536
+# export HADOOP_HEAPSIZE=65536
+export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Xmx65536m -Dhadoop.root.logger=INFO,RFA"
 
-cd ../hadoop-hdfs-project/hadoop-hdfs
-mvn test -e -Dtest=TestMetaSave#testMetaSaveWithLargeUnderReplicate
+#rm -rf ../hadoop-hdfs-project/hadoop-hdfs/target/test/data/
+#cd ../hadoop-hdfs-project/hadoop-hdfs
+#mvn test -e -Dtest=TestMetaSave#testMetaSaveWithLargeUnderReplicate
 
-#sleep 5
-#echo "methodTraceDump=true" > $SFIELD_INPUT
+hadoop org.apache.hadoop.hdfs.server.namenode.TestMetaSaveScale 
 
